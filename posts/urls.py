0528e4776dbbf84d3ef574f95cea31e15f570599
paullabkorea <paullabkorea@gmail.com>
2023-12-07 17:15:40 +0900

    1207

--- posts/urls.py
+++ posts/urls.py
@@ -9,10 +9,10 @@ from .views import (
 )
 
 urlpatterns = [
-    path('posts/', PostCreateView.as_view(), name='post-create'),
-    path('comments/', CommentCreateView.as_view(), name='comment-create'),
-    path('posts/<int:post_id>/comments/', CommentListView.as_view(), name='comment-list'),
-    path('posts/<int:post_id>/like/', LikeView.as_view(), name='post-like'),
-    path('posts/list/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('list/', PostListView.as_view(), name='post-list'), # 게시물 리스트
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'), # 게시물 상세보기
+    path('create/', PostCreateView.as_view(), name='post-create'), # 게시물 생성
+    path('comments/', CommentCreateView.as_view(), name='comment-create'), # 댓글 생성
+    path('<int:post_id>/comments/', CommentListView.as_view(), name='comment-list'), # 댓글 리스트
+    path('<int:post_id>/like/', LikeView.as_view(), name='post-like'),# 게시물에 대한 좋아요
 ]
